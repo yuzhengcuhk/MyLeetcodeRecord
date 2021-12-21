81dfbcd1f5ea62e1fca14f1779496b6df054376a
Yu Zheng <zy018@ie.cuhk.edu.hk>
2021-12-21 21:09:41 +0800

    Time: 164 ms (77.61%), Space: 15.7 MB (11.32%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+       
+        majorBucket = {}
+        counter = len(nums)
+        
+        for i in range(0, counter):
+            if nums[i] in majorBucket:
+                majorBucket[nums[i]] = majorBucket[nums[i]] + 1
+            else:
+                majorBucket[nums[i]] = 1
+             
+        resCounter = 0
+        result = None
+        
+        for key, value in majorBucket.items():
+            if value > resCounter:
+                result = key
+                resCounter = value
+        
+        return result
