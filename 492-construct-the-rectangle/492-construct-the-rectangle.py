e80a38c9c33d3cdb415ad812360c6539fd220006
Yu Zheng <zy018@ie.cuhk.edu.hk>
2022-02-06 19:38:18 +0800

    Time: 42 ms (59.56%), Space: 13.9 MB (85.29%) - LeetHub

--- 492-construct-the-rectangle/492-construct-the-rectangle.py
+++ 492-construct-the-rectangle/492-construct-the-rectangle.py
@@ -1,7 +1,6 @@
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
         widthMax = int(area**0.5)
-        result = []
         for recWidth in range(widthMax,1,-1):
             recLength = area // recWidth
             if recLength * recWidth == area:
