bd114f8b46a4501214aa3e5824dd74381b73f9e7
Yu Zheng <zy018@ie.cuhk.edu.hk>
2022-03-02 23:09:27 +0800

    Time: 275 ms (58.48%), Space: 15.9 MB (28.17%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        sumCorrect = len(nums)*(len(nums)+1) / 2
+        numRepeat = sum(nums)-sum(set(nums))
+        return [numRepeat, int(sumCorrect-sum(set(nums)))]
